4bbc52a4e38d93659c44979c06b73d13d449f6d8
chongh <304451676@qq.com>
2018-10-30 15:50:31 +0800

    tree

---
+++ data_structure/problems/L20.c
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: L20.c
+	> Author: ChongH
+	> Mail: 304451676@qq.com
+	> Created Time: 2018年10月25日 星期四 19时04分40秒
+ ************************************************************************/
+
+#include <stdio.h>
+#define MAX 100
+int flag = 0;
+void dfs(char ch, int step) {
+    if (step =)
+}
+
+
+int main() {
+    char str[MAX];
+    scanf("%s", str);
+
+    return 0;
+}
