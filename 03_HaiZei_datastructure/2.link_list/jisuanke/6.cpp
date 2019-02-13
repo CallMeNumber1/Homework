488c1872e049b4519db3f63404b0cf963116b82d
chongh <304451676@qq.com>
2019-02-13 23:12:35 +0800

    linklist practice

---
+++ 03_HaiZei_datastructure/2.link_list/jisuanke/6.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: 6.cpp
+	> Author: ChongH
+	> Mail: 304451676@qq.com
+	> Created Time: 2019年02月13日 星期三 23时10分29秒
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+typedef struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+} Node;
+
+int main() {
+    int n, val, m;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+    }
+    
+
+    return 0;
+}
